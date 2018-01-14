5b433f017bcc689360f4390bde786634fe7f2fd3
Yilisav <vik3000@list.ru>
2018-01-14 22:37:43 +0500

    lab 4
    4 лр: Работа с СУБД.
    Необходимо показать владение языком SQL или какой-либо ORM (django ...)
    Знание того, какие проблемы безопасности возникают при работе с СУБД.
    Умение подключать сервер БД к веб-приложению, создавать репликации, переносить
    сервер на другое оборудование.

--- input.php
+++ input.php
@@ -1,12 +1,21 @@
 ﻿<?php
+	include("res.php");
 	$str="";
 	//если существуют переменные
 	if(isset($_POST["fio"], $_POST["mail"], $_POST["question"])){
+		$fio= ($_POST["fio"]);
+		$mail = ($_POST["mail"]);
+		$question = ($_POST["question"]);
+		// вставка в базу
+		$query = mysql_query("insert into res (fio, mail, question) values ('".$fio."','".$mail."','".$question."')");
+		if ($query == 'true') {
 			$str = "Ваш вопрсоc успешно отправлен!";
 		} else {
-			$str = "Возникла ошибка!";
-				exit;
+			$str = "Возникла ошибка!".mysql_error();
+		}
+	}else{
+		echo "Не введены данные. Попробуйте еще раз.";
+		exit;
 	}
 	echo $str;
-	
 ?>
---
+++ res.php
@@ -0,0 +1,6 @@
+﻿
+<?php 
+$db = mysql_connect("localhost","root","");
+mysql_select_db("res",$db);
+mysql_query('SET NAMES utf8', $db); 
+?>
