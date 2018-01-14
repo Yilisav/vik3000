bd1bbaf449edf21340842264148c561acd880853
Yilisav <vik3000@list.ru>
2018-01-14 22:06:51 +0500

    lab 3
    3 лр: Обработка формы обратной связи на стороне сервера приложений.
    Необходимо показать владение PhP (Python, C#, Perl, NodeJS или иным АЯП, применяемым для веб-разработки).
    Знание того, как происходит передача данных между браузером и сервером.
    Умение работать с протоколом HTTP в синхронном и асинхронном режимах.

---
+++ input.php
@@ -0,0 +1,12 @@
+﻿<?php
+	$str="";
+	//если существуют переменные
+	if(isset($_POST["fio"], $_POST["mail"], $_POST["question"])){
+			$str = "Ваш вопрсоc успешно отправлен!";
+		} else {
+			$str = "Возникла ошибка!";
+				exit;
+	}
+	echo $str;
+	
+?>
